7e13777a12232b0a8c6f6d353d6415bb5ece74d6
MAMGUE TAGNE Lucrece Darylle <lucrece.mamgue@groupe-esigelec.org>
2025-01-10 16:50:03 +0100

    2eme spider

---
+++ projetscrapy/projetscrapy/spiders/nom_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+#from ..items import ProjetscrapyItem
+
+class NomSpiderSpider(scrapy.Spider):
+    name = 'nom_spider'
+    allowed_domains = ['www.mobileshop.eu']
+    start_urls = ['https://www.mobileshop.eu/fr/ios-os/']
+
+    def parse(self, response):
+        phones = response.xpath('//div[@class="product-module"]')
+        for phone in phones :
+            yield{
+                "nom": phone.xpath('.//div[@class="product-name"]/h5/a/text()').get(),
+                "price": phone.xpath('.//div[@class="price"]/div/text()').get(),
+                "lien": phone.xpath('.//div[@class="product-name"]/h5/a/@href').get(),
+            }
+
+        next_page = response.xpath('//nav[@class="pager"]/div/div/a[2]/@href').get()
+        if next_page is not None:
+            next_page_url = response.urljoin(next_page)
+            yield scrapy.Request(next_page_url)
